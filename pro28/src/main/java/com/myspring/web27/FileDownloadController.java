fed9fc70d118931d741c6a31c22c841984e7f0b2
장은빈 <dmsqls@ZEB>
2023-09-08 15:28:10 +0900

    pro28_커밋

---
+++ pro28/src/main/java/com/myspring/web27/FileDownloadController.java
@@ -0,0 +1,64 @@
+package com.myspring.web27;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import net.coobird.thumbnailator.Thumbnails;
+
+@Controller
+public class FileDownloadController {
+	
+	private static String CURR_IMAGE_REPO_PATH = "c:\\spring\\image_repo";
+	
+	// 썸네일 이미지 사용하기
+	@RequestMapping("/download")
+	protected void download(@RequestParam("imageFileName") String imageFileName, HttpServletResponse response) throws Exception {
+		OutputStream out = response.getOutputStream();
+		String filePath = CURR_IMAGE_REPO_PATH + "\\" + imageFileName;
+		File image = new File(filePath);
+		int lastIndex = imageFileName.lastIndexOf(".");
+		String fileName = imageFileName.substring(0, lastIndex);
+		File thumbnail = new File(CURR_IMAGE_REPO_PATH + "\\" + "thumbnail" + "\\" + fileName + ".png");
+		if(image.exists()) {
+			thumbnail.getParentFile().mkdirs();
+			Thumbnails.of(image).size(50, 50).outputFormat("png").toFile(thumbnail);
+		}
+		
+		FileInputStream in = new FileInputStream(thumbnail);
+		byte[] buffer = new byte[1024 * 8];
+		while(true) {
+			int count = in.read(buffer);
+			if(count == -1) 
+				break;
+				out.write(buffer, 0, count);
+		}
+		in.close();
+		out.close();
+	}
+
+		//웹출력 부분, 여기 기능은 단일기능, jsp에서 forEach로 여러번 돌리니까
+//	  @RequestMapping("/download")
+//	  protected void download(@RequestParam("imageFileName") String imageFileName, HttpServletResponse response) throws Exception { 
+//		  OutputStream out = response.getOutputStream();
+//		  String downFile = CURR_IMAGE_REPO_PATH + "\\" + imageFileName; File file = new File(downFile);
+//		  response.setHeader("Cache-Control", "no-cache");
+//		  response.addHeader("Content-disposition", "attachment; fileName=" + imageFileName);
+//		  FileInputStream in = new FileInputStream(file);
+//		  byte[] buffer = new byte[1024 * 8]; while(true) {
+//			  int count = in.read(buffer);
+//			  if (count == -1) break;
+//			  out.write(buffer, 0, count);
+//		} 
+//		in.close();
+//		out.close();
+//	}
+	 
+
+}
